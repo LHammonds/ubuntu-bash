f97c32b07d59959c202e77d7fcbf9daf06c29dea
LHammonds <Lon.Hammonds@gmail.com>
2020-08-20 09:25:56 -0500

    Create servicestart.sh

---
+++ servicestart.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+#############################################################
+## Name          : servicestart.sh
+## Version       : 1.0
+## Date          : 2013-01-08
+## Author        : LHammonds
+## Compatibility : Ubuntu Server 12.04 LTS
+## Requirements  : None
+## Purpose       : Cleanly start primary services.
+## Run Frequency : As needed
+## Exit Codes    : None
+###################### CHANGE LOG ###########################
+## DATE       VER WHO WHAT WAS CHANGED
+## ---------- --- --- ---------------------------------------
+## 2013-01-08 1.0 LTH Created script.
+#############################################################
+clear
+echo ""
+echo "Starting services..."
+#service mysql start
+#service apache2 start
+sleep 1
