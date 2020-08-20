600c2b0bd9fe2cfff03d7f26bf2444a1ad7d3a21
LHammonds <Lon.Hammonds@gmail.com>
2020-08-20 09:24:48 -0500

    Create servicestop.sh

---
+++ servicestop.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+#############################################################
+## Name          : servicestop.sh
+## Version       : 1.0
+## Date          : 2013-01-08
+## Author        : LHammonds
+## Compatibility : Ubuntu Server 12.04 LTS
+## Requirements  : None
+## Purpose       : Cleanly stop primary services.
+## Run Frequency : As needed
+## Exit Codes    : None
+###################### CHANGE LOG ###########################
+## DATE       VER WHO WHAT WAS CHANGED
+## ---------- --- --- ---------------------------------------
+## 2013-01-08 1.0 LTH Created script.
+#############################################################
+clear
+echo ""
+echo "Stopping services..."
+#service apache2 stop
+#service mysql stop
+sleep 1
