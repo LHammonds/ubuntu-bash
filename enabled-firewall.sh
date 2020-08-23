693c105cec1d39f1008705fd94c1e92b84b2bfee
LHammonds <Lon.Hammonds@gmail.com>
2020-08-23 16:53:57 -0500

    Update enabled-firewall.sh

--- enabled-firewall.sh
+++ enabled-firewall.sh
@@ -4,7 +4,7 @@
 ## Version       : 1.1
 ## Date          : 2017-04-13
 ## Author        : LHammonds
-## Compatibility : Ubuntu Server 12.04 - 18.04 LTS
+## Compatibility : Ubuntu Server 12.04 - 20.04 LTS
 ## Requirements  : Run as root
 ## Purpose       : Restore and enable firewall.
 ## Run Frequency : As needed
