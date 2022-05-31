6ad9bd88ff3bba893845e218b2ae18ad69a0ee7c
LHammonds <Lon.Hammonds@gmail.com>
2022-05-31 15:51:20 -0500

    Verified on 22.04

--- servicerestart.sh
+++ servicerestart.sh
@@ -1,20 +1,20 @@
 #!/bin/bash
 #############################################################
 ## Name          : servicerestart.sh
-## Version       : 1.0
-## Date          : 2013-01-08
+## Version       : 1.1
+## Date          : 2018-04-19
 ## Author        : LHammonds
-## Compatibility : Ubuntu Server 12.04 - 20.04 LTS
+## Compatibility : Verified on Ubuntu Server 22.04 LTS
 ## Requirements  : None
-## Purpose       : Cleanly stop/start primary services.
+## Purpose       : Stop/Start primary services.
 ## Run Frequency : As needed
 ## Exit Codes    : None
 ###################### CHANGE LOG ###########################
 ## DATE       VER WHO WHAT WAS CHANGED
 ## ---------- --- --- ---------------------------------------
 ## 2013-01-08 1.0 LTH Created script.
+## 2018-04-19 1.1 LTH Spit stop/start code into individual scripts.
 #############################################################
-
 ## Import standard variables and functions. ##
 source /var/scripts/common/standard.conf
 clear
